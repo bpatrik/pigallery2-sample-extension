975b6ba5b277214396000ae15d73ad2005336443
Patrik J. Braun <bra.patrik@gmail.com>
2025-10-24 00:26:23 +0200

    Update sample extension with fake button on 4+ star photos

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ npm run build
 ```
 
 Also see extension documentation at https://github.com/bpatrik/pigallery2/tree/master/extension
+
+------------------------------------------------------------------------
+
+This sample extension adds the following and more: 
+
+Three new buttons to the top left of the photo:
+ * Great photo if the photo has 4+ stars
+ * edit button for editing a photo
+ * delete button for deleting a photo (only form db)
+ * 
+![Added buttons](buttons.png)
--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@
 import {IExtensionObject} from './node_modules/pigallery2-extension-kit';
 import {PhotoMetadata} from './node_modules/pigallery2-extension-kit/lib/common/entities/PhotoDTO';
 import {UserRoles} from './node_modules/pigallery2-extension-kit/lib/common/entities/UserDTO';
+import {UserDTO} from './node_modules/pigallery2-extension-kit/lib/common/entities/UserDTO';
+import {MediaEntity} from './node_modules/pigallery2-extension-kit/lib/backend/model/database/enitites/MediaEntity';
 
 // Including prod extension packages. You need to prefix them with ./node_modules
 // lodash does not have types
@@ -14,8 +16,6 @@ import * as _ from './node_modules/lodash';
 import {Column, Entity, Index, PrimaryGeneratedColumn, Repository} from 'typeorm';
 import {TestConfig} from './config';
 import {ParamsDictionary} from 'express-serve-static-core';
-import {UserDTO} from 'pigallery2-extension-kit/lib/common/entities/UserDTO';
-import {MediaEntity} from 'pigallery2-extension-kit/lib/backend/model/database/enitites/MediaEntity';
 
 // Using typeorm for ORM
 @Entity()
@@ -174,6 +174,20 @@ export const init = async (extension: IExtensionObject<TestConfig>): Promise<voi
     }
   });
 
+
+  /**
+   * (Optional) Adding a (non-clickable) button to all photos with 4+ stars
+   * Note: button order matters, but always visible buttons will be shown first
+   */
+  extension.ui.addMediaButton({
+    name: 'Great Photo',
+    svgIcon: {
+      viewBox: '0 0 640 640',
+      items: '<path d="M341.5 45.1C337.4 37.1 329.1 32 320.1 32C311.1 32 302.8 37.1 298.7 45.1L225.1 189.3L65.2 214.7C56.3 216.1 48.9 222.4 46.1 231C43.3 239.6 45.6 249 51.9 255.4L166.3 369.9L141.1 529.8C139.7 538.7 143.4 547.7 150.7 553C158 558.3 167.6 559.1 175.7 555L320.1 481.6L464.4 555C472.4 559.1 482.1 558.3 489.4 553C496.7 547.7 500.4 538.8 499 529.8L473.7 369.9L588.1 255.4C594.5 249 596.7 239.6 593.9 231C591.1 222.4 583.8 216.1 574.8 214.7L415 189.3L341.5 45.1z"/></svg>'
+    },
+    metadataFilter: [{field: 'rating', comparator: '>=', value: 4}],
+    alwaysVisible: true
+  });
 
   /**
    * (Optional) Creating a messenger. You can use it with TopPickJob to send photos
